f62e9ca9d960486114a6a6bb00dea65565874c8e
Pruthvi <pruthvi@Pruthvis-MacBook-Pro.local>
2023-08-08 09:50:20 -0400

    hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,4 +1,38 @@
 const express=require('express')
+const path=require("path")
+
+const publicDirectory=path.join(__dirname,"../public")
 
 const app=express()
 
+app.use(express.static(publicDirectory))
+
+app.set("view engine","hbs")
+
+app.get("/",(req,res)=>{
+    res.render('index',{
+        title:'Weather',
+        name:"Pruthvi"
+    })
+})
+
+app.get("/help",(req,res)=>{
+    res.render("help",{
+        name:"pruthvi"
+    })
+})
+
+app.get("/about",(req,res)=>{
+    res.render("about",{
+        title:"About",
+        name:"Pruthvi"
+    })
+})
+
+app.get("/weather",(req,res)=>{
+    res.send("weather")
+})
+
+app.listen(3000,()=>{
+    console.log("Server is up on port : ",3000)
+})
